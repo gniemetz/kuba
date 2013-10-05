82b6345acd3424fa440473f4aa1f44b7002a4fe2
Gerd Niemetz <gerd@niemetz.it>
2013-10-05 16:55:28 +0200

    First update

--- web2py/applications/kuba/models/menu.py
+++ web2py/applications/kuba/models/menu.py
@@ -5,19 +5,18 @@
 ## Customize your APP title, subtitle and menus here
 #########################################################################
 
-response.logo = A(B('web',SPAN(2),'py'),XML('&trade;&nbsp;'),
-                  _class="brand",_href="http://www.web2py.com/")
-response.title = request.application.replace('_',' ').title()
-response.subtitle = ''
+response.logo = A(B('KUBA'), _class="brand", _href="http://www.web2py.com/")
+response.title = request.application.replace('_', ' ').title()
+response.subtitle = 'KUlturBAhnhof Eferding'
 
 ## read more at http://dev.w3.org/html5/markup/meta.name.html
-response.meta.author = 'Your Name <you@example.com>'
-response.meta.description = 'a cool new app'
-response.meta.keywords = 'web2py, python, framework'
+response.meta.author = 'Gerd Niemetz <gerd@niemetz.it>'
+response.meta.description = 'KUBA - gemeinnütziger Kulturverein mit Events im Bereich Musik, Film, Kabarett, etc. am Bahnhof Eferding'
+response.meta.keywords = 'KUBA, Kulturbahnhof, Kultur, Events, Musik, Livemusik, Film, Filmabend, Kabarett'
 response.meta.generator = 'Web2py Web Framework'
 
 ## your http://google.com/analytics id
-response.google_analytics_id = None
+response.google_analytics_id = 'UA-29404706-1'
 
 #########################################################################
 ## this is the main application menu add/remove items as required
